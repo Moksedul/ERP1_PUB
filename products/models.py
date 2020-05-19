c4d50d4554cde99b8969ae36a4ed0b6f3a968075
moksedul69 <mokseduln@gmail.com>
2020-05-19 15:29:56 +0600

    products model added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    product_name = models.CharField(max_length=200)
+    product_category = models.CharField(max_length=200)
+
