5fa986d68ba80e6679bd887d707ac9a34901efc5
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-06-07 19:18:23 +0600

    added investment  function

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
 
 
-class BankAccounts(models.Model):
+class Accounts(models.Model):
     account_name = models.CharField(max_length=50, null=True, default='M/S FATEMA ENTERPRISE')
-    account_no = models.CharField(max_length=50, null=True, unique=True)
-    bank_name = models.CharField(max_length=50, null=True)
-    bank_branch = models.CharField(max_length=50, null=True)
+    account_no = models.CharField(max_length=50, null=True, unique=True, blank=True)
+    bank_name = models.CharField(max_length=50, null=True, blank=True)
+    bank_branch = models.CharField(max_length=50, null=True, blank=True)
     opening_balance = models.FloatField(null=True, default=0)
     remarks = models.CharField(max_length=200, null=True, blank=True)
 
@@ -20,23 +19,10 @@ class BankAccounts(models.Model):
         return reverse('bank-account-list')
 
 
-class OtherAccounts(models.Model):
-    account_name = models.CharField(max_length=50, null=True, default='CASH IN HAND', unique=True)
-    opening_balance = models.FloatField(null=True, default=0)
-    remarks = models.CharField(max_length=200, null=True, blank=True)
-
-    def __str__(self):
-        return self.account_name
-
-    @staticmethod
-    def get_absolute_url():
-        return reverse('other-account-list')
-
-
 class Investment(models.Model):
     source_of_investment = models.CharField(max_length=200)
     investing_amount = models.FloatField()
-    investing_to_account = GenericForeignKey('BankAccounts', 'OtherAccounts')
+    investing_to_account = models.ForeignKey(Accounts, null=True, on_delete=models.CASCADE)
     added_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     remarks = models.CharField(max_length=200, null=True, blank=True)
 
@@ -45,4 +31,6 @@ class Investment(models.Model):
 
     @staticmethod
     def get_absolute_url():
-        return reverse('investment-list')
+        return reverse('bank-account-list')
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import *
@@ -11,51 +12,56 @@ def home(request):
 
 
 class BankAccountCreate(LoginRequiredMixin, CreateView):
-    model = BankAccounts
+    model = Accounts
     template_name = 'accounts/bank_account_add_form.html'
     fields = '__all__'
 
 
 class BankAccountList(LoginRequiredMixin, ListView):
-    model = BankAccounts
+    model = Accounts
     template_name = 'accounts/bank_account_list.html'
     context_object_name = 'accounts'
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super(BankAccountList, self).get_context_data(**kwargs)
+        context['remaining_balance'] = '50000'
+        return context
+
 
 class BankAccountUpdate(LoginRequiredMixin, UpdateView):
-    model = BankAccounts
+    model = Accounts
     template_name = 'accounts/bank_account_update_form.html'
     fields = '__all__'
 
 
 class BankAccountDelete(LoginRequiredMixin, DeleteView):
-    model = BankAccounts
+    model = Accounts
     template_name = 'accounts/bank_account_delete.html'
     success_url = '/bank_account_list'
 
 
 class OtherAccountCreate(LoginRequiredMixin, CreateView):
-    model = OtherAccounts
+    model = Accounts
     template_name = 'accounts/other_account_add_form.html'
-    fields = '__all__'
+    fields = ('account_name', 'opening_balance', 'remarks')
 
 
 class OtherAccountList(LoginRequiredMixin, ListView):
-    model = OtherAccounts
+    model = Accounts
     template_name = 'accounts/other_account_list.html'
     context_object_name = 'accounts'
     paginate_by = 5
 
 
 class OtherAccountUpdate(LoginRequiredMixin, UpdateView):
-    model = OtherAccounts
+    model = Accounts
     template_name = 'accounts/other_account_update_form.html'
     fields = '__all__'
 
 
 class OtherAccountDelete(LoginRequiredMixin, DeleteView):
-    model = OtherAccounts
+    model = Accounts
     template_name = 'accounts/other_account_delete.html'
     success_url = '/other_account_list'
 
@@ -67,4 +73,6 @@ class InvestmentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.added_by = self.request.user
+        account = Accounts.objects.get(pk=form.cleaned_data['investing_to_account'].id)
+        print(account.bank_name)
         return super().form_valid(form)
