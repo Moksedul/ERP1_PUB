b361c7ce2b7f7c0cf79e44def75220f4c81f0614
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-06-13 01:57:11 +0600

    payment search option added

--- payments/urls.py
+++ payments/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('add_payment', PaymentCreate.as_view(), name='add-payment'),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('payment_list', PaymentListView.as_view(), name='payment-list'),
-    path('payment/<int:pk>/detail', PaymentDetailView.as_view(), name='payment-detail'),
+    path('payment/<int:pk>/detail', payment_details, name='payment-detail'),
     path('payment/<int:pk>/update', PaymentUpdateView.as_view(), name='payment-update'),
     path('payment/<int:pk>/delete', PaymentDeleteView.as_view(), name='payment-delete'),
+    path('payment_search/', payment_search, name='payment-search'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+import string
+from num2words import num2words
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import Payment
@@ -37,12 +40,32 @@ class PaymentUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'payments/payment_update_form.html'
 
 
-class PaymentDetailView(LoginRequiredMixin, DetailView):
-    model = Payment
-    # template_name = 'payments/payment_Detail.html'
+def payment_details(request, pk):
+    payment = Payment.objects.get(id=pk)
+    payed_amount_word = string.capwords(num2words(payment.payment_amount))
+    context = {
+        'payment': payment,
+        'payed_amount_word': payed_amount_word,
+    }
+    return render(request, 'payments/payment_Detail.html', context)
 
 
 class PaymentDeleteView(LoginRequiredMixin, DeleteView):
     model = Payment
     template_name = 'payments/payment_confirm_delete.html'
     success_url = '/payment_list'
+
+
+def payment_search(request):
+    payments = Payment.objects.all()
+    name_contains_query = request.GET.get('name_contains')
+
+    if name_contains_query != '' and name_contains_query is not None:
+        payments = payments.filter(payed_to__contains=name_contains_query)
+    paginator = Paginator(payments, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(request, "payments/payment_search_form.html", context)
