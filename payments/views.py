db6712d09cc7679b0aae636f9c634cc48ce7dcd1
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-06-14 19:24:51 +0600

    payment search option added

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.core.paginator import Paginator
 import string
 from num2words import num2words
@@ -58,21 +59,23 @@ class PaymentDeleteView(LoginRequiredMixin, DeleteView):
 
 
 def payment_search(request):
-    buy_voucher = BuyVoucher.objects.all()
+    buy_vouchers = BuyVoucher.objects.all()
     payments = Payment.objects.all()
     name_contains_query = request.GET.get('name_contains')
     voucher_contains_query = request.GET.get('voucher_no')
 
     if name_contains_query != '' and name_contains_query is not None:
         payments = payments.filter(payed_to__contains=name_contains_query)
-    if name_contains_query == '' and voucher_contains_query is not None:
-        buy_voucher = buy_voucher.filter(voucher_number=voucher_contains_query)
-        payments = payments.filter(buy_voucher)
+    elif voucher_contains_query != '' and voucher_contains_query is not 'Choose...':
+        buy_voucher = buy_vouchers.filter(voucher_number=voucher_contains_query)
+        for voucher in buy_voucher:
+            payments = payments.filter(voucher_no_id=voucher.id)
+
     paginator = Paginator(payments, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
-        'vouchers': buy_voucher
+        'vouchers': buy_vouchers
     }
     return render(request, "payments/payment_search_form.html", context)
