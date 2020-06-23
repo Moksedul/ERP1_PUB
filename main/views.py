b0cba146b529382d2656b38e2ab623f90f86ce14
moksedul69 <mokseduln@gmail.com>
2020-06-23 17:51:33 +0600

    home page editing

--- main/views.py
+++ main/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from orders.models import Orders
 
 
 # Create your views here.
 @login_required()
 def home(request):
-    return render(request, 'main/home.html')
+    order_count = Orders.objects.all().count()
+    context = {
+        'order_count': order_count
+    }
+    return render(request, 'main/home.html', context)
 
 
 @login_required()
 def settings(request):
     return render(request, 'main/settings_home.html')
+
