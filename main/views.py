9919bf52eb29f2d6d4836749fa71fb6e02f1b97b
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-06-24 09:11:34 +0600

    payment search option added

--- main/views.py
+++ main/views.py
@@ -1,15 +1,37 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from orders.models import Orders
+from vouchers.models import SaleVoucher, BuyVoucher
+from stocks.models import Stocks
 
 
 # Create your views here.
 @login_required()
 def home(request):
-    order_count = Orders.objects.all().count()
+    orders = Orders.objects.all()
+    stocks = Stocks.objects.all()
+    order_count = orders.count()
+    sale_count = SaleVoucher.objects.all().count()
+    buy_count = BuyVoucher.objects.all().count()
+
+    # limiting stocks items to show
+    paginator = Paginator(stocks, 3)
+    page_number = request.GET.get('page')
+    stocks = paginator.get_page(page_number)
+
+    # limiting order items to show
+    paginator = Paginator(orders, 3)
+    page_number = request.GET.get('page')
+    orders = paginator.get_page(page_number)
+
     context = {
-        'order_count': order_count
+        'order_count': order_count,
+        'sale_count': sale_count,
+        'buy_count': buy_count,
+        'orders': orders,
+        'stocks': stocks
     }
     return render(request, 'main/home.html', context)
 
--- stocks/models.py
+++ stocks/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 from products.models import Products
 
 CONDITION_CHOICES = [
-    ('pr', 'Processed'),
-    ('upr', 'Un Processed'),
+    ('Processed', 'Processed'),
+    ('Un Processed', 'Un Processed'),
 ]
 
 
@@ -26,7 +26,7 @@ class Stocks(models.Model):
     number_of_bag = models.FloatField()
     weight = models.FloatField()
     name_of_product = models.ForeignKey(Products, on_delete=models.CASCADE, null=True)
-    product_condition = models.CharField(max_length=50, choices=CONDITION_CHOICES, default='pr')
+    product_condition = models.CharField(max_length=50, choices=CONDITION_CHOICES, default='Processed')
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     remarks = models.CharField(max_length=200, blank=True)
 
