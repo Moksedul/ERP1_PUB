663cdd8129c086846cc58235d65b52563a2d9603
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-06-29 08:52:50 +0600

    ledger programing

--- ledger/views.py
+++ ledger/views.py
@@ -2,12 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from vouchers.models import GeneralVoucher
 from collection.models import Collection
+from payments.models import Payment
 
 
 @login_required()
 def index(request):
     general_vouchers = GeneralVoucher.objects.all()
     collections = Collection.objects.all()
+    payments = Payment.objects.all()
 
     ledgers = {
 
@@ -18,12 +20,38 @@ def index(request):
         ledgers.setdefault(key, [])
         ledgers[key].append({
             'date': voucher.date_added,
-            'voucher_no': voucher.voucher_number,
+            'voucher_no': voucher.voucher_number + ' General Cost to ' + voucher.person_name,
             'descriptions': voucher.cost_Descriptions,
-            'amount': voucher.cost_amount
+            'debit_amount': voucher.cost_amount
         })
 
+    for voucher in collections:
+        key = "voucher"
+        ledgers.setdefault(key, [])
+        ledgers[key].append({
+            'date': voucher.collection_date,
+            'voucher_no': voucher.collection_no + ' Collection',
+            'descriptions': voucher.sale_voucher_no,
+            'credit_amount': voucher.collection_amount
+        })
+
+    for voucher in payments:
+        key = "voucher"
+        ledgers.setdefault(key, [])
+        ledgers[key].append({
+            'date': voucher.payment_date,
+            'voucher_no': voucher.payment_no + ' Payment',
+            'descriptions': voucher.voucher_no,
+            'debit_amount': voucher.payment_amount
+        })
+
+    def my_function(e):
+        return e['date']
+
+    shorting_by_date = ledgers['voucher'].sort(key=my_function, reverse=True)
+
     context = {
+        'shorting_by_date': shorting_by_date,
         'ledgers': ledgers['voucher']
     }
 
