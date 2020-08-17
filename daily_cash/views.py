742c6eb098abee07baffbf75633c2253b89ddb50
moksedul69 <mokseduln@gmail.com>
2020-08-17 18:21:15 +0600

    editing daily cash

--- daily_cash/views.py
+++ daily_cash/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import DailyCash
 
 
 def create(data):
+    daily_cash = DailyCash(payment_voucher=1, name='Physics', max_marks=100)
     print(data)
+
