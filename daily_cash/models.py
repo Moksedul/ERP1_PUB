2b2cb013ff9b28413df57a34a47147912d59377d
moksedul69 <mokseduln@gmail.com>
2020-08-18 17:22:38 +0600

    editing daily cash

--- daily_cash/models.py
+++ daily_cash/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from payments.models import Payment
 from collection.models import Collection
 from vouchers.models import *
+from accounts.models import Investment
 
 
 class DailyCash(models.Model):
     date = models.DateField(auto_now=True)
-    voucher = models.ForeignKey(Payment, GeneralVoucher, Collection, on_delete=models.DO_NOTHING, null=True, blank=True)
+    general_voucher = models.ForeignKey(GeneralVoucher, on_delete=models.CASCADE, null=True, blank=True)
+    payment_no = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True, blank=True)
+    collection_no = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
+    investment_no = models.ForeignKey(Investment, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(max_length=1000)
     type = models.CharField(max_length=10)
--- daily_cash/views.py
+++ daily_cash/views.py
@@ -3,7 +3,7 @@ from .models import DailyCash
 
 
 def create_daily_cash(data):
-    daily_cash = DailyCash(voucher=data['voucher'], description=data['description'], type=data['type'])
+    daily_cash = DailyCash(general_voucher=data['voucher'], description=data['description'], type=data['type'])
     # daily_cash.save()
     print(daily_cash)
 
