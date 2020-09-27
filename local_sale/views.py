141211e643e6892ea02a682fbb2def1c21008754
Moksedul Alam <mokseduln@gmail.com>
2020-09-27 17:54:47 +0600

    version 1.40

--- local_sale/views.py
+++ local_sale/views.py
@@ -1,3 +1,24 @@
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import LocalSale, Product
+
 
 # Create your views here.
+class SaleForm(ModelForm):
+    class Meta:
+        model = LocalSale
+
+
+def sale_create(request):
+    context = {}
+
+    if request.method == 'POST':  # If the form has been submitted...
+        sale_form = SaleForm(request.POST, prefix="sale")
+        # b_form = BForm(request.POST, prefix="b")
+        # c_form = CForm(request.POST, prefix="c")
+        if sale_form.is_valid():  # All validation rules pass
+            print("all validation passed")
+
+    context['form'] = form
+    return render(request, 'vouchers/general_voucher_add_form.html', context)
