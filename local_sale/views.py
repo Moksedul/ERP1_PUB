1d228a89a386774562f69e96bcb7dd1325c5b9df
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-09-28 08:46:47 +0600

    Version: 1.04

--- local_sale/views.py
+++ local_sale/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.forms import ModelForm
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import LocalSale, Product
 
 
@@ -8,17 +9,17 @@ from .models import LocalSale, Product
 class SaleForm(ModelForm):
     class Meta:
         model = LocalSale
+        fields = '__all__'
 
 
 def sale_create(request):
-    context = {}
-    sale_form = {}
-    if request.method == 'POST':  # If the form has been submitted...
-        sale_form = SaleForm(request.POST, prefix="sale")
-        # b_form = BForm(request.POST, prefix="b")
-        # c_form = CForm(request.POST, prefix="c")
-        if sale_form.is_valid():  # All validation rules pass
-            print("all validation passed")
-
-    context['form'] = sale_form
-    return render(request, 'local_sale/sale_add_form.html', context)
+    if request.method == 'POST':
+        form = SaleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Account Has Been Created ! You can now log in.')
+            return redirect('#')
+    else:
+        form = SaleForm()
+    return render(request, 'local_sale/sale_add_form.html', {'form': form})
