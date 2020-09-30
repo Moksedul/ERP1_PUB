1b6972eb2b0fd6283fb23b81a53f8b2e2b0a8321
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-09-30 08:44:40 +0600

    Version: 1.04

--- local_sale/urls.py
+++ local_sale/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.i18n import JavaScriptCatalog
-from .views import sale_create, SaleCreate, LocalSaleList, LocalSaleDelete
+from .views import sale_create, LocalSaleList, LocalSaleDelete, sale_update
 
 urlpatterns = [
     path('add_local_sale', sale_create, name='add-local-sale'),
     path('local_sale_list', LocalSaleList.as_view(), name='local-sale-list'),
     path('local_sale/<int:pk>/delete', LocalSaleDelete.as_view(), name='delete-local-sale'),
+    path('local_sale/<int:pk>/update', sale_update, name='update-local-sale'),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
--- local_sale/views.py
+++ local_sale/views.py
@@ -1,23 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, ListView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DeleteView
 from django.forms import formset_factory, inlineformset_factory
 from .forms import SaleForm, ProductForm
 from .models import LocalSale, Product
-
-
-class SaleCreate(LoginRequiredMixin, CreateView):
-    form_class = SaleForm
-    template_name = 'local_sale/sale_add_form.html'
-    success_url = '/add_local_sale'
-
-    def form_valid(self, form):
-        form.instance.posted_by = self.request.user
-        super().form_valid(form=form)
-        print(form)
-        return HttpResponseRedirect(self.get_success_url())
 
 
 @login_required
@@ -45,11 +32,17 @@ def sale_create(request):
 @login_required
 def sale_update(request, pk):
     sale = LocalSale.objects.get(pk=pk)
+    product = Product.objects.filter(sale_no_id=sale.id)
     form1 = SaleForm(instance=sale)
-    product_set = inlineformset_factory(LocalSale, Product, fields=('name',))
+    product_set = inlineformset_factory(LocalSale, Product, fields=('name', 'rate', 'weight'))
     form2set = product_set(instance=sale)
     if request.method == 'POST':
+        print('in post')
+        form1 = SaleForm(request.POST or None, instance=sale)
+        product_set = inlineformset_factory(LocalSale, Product, fields=('name', 'rate', 'weight'))
+        form2set = product_set(request.POST or None, request.FILES or None)
         if form1.is_valid():
+            print('valid')
             sale = form1.save(commit=False)
             sale.posted_by = request.user
             sale.save()
@@ -59,11 +52,15 @@ def sale_update(request, pk):
                 product.save()
             return redirect('/local_sale_list')
     else:
-        form1 = SaleForm
-        form2set = product_set
+        form1 = form1
+        form2set = form2set
 
     return render(request, 'local_sale/sale_add_form.html', {'form1': form1, 'form2set': form2set})
 
+
+def local_sale_detail(request, pk):
+    sale = LocalSale.object.get(id=pk)
+    products = Product.objects.filter()
 
 class LocalSaleList(LoginRequiredMixin, ListView):
     model = LocalSale
