f419dca826074e627a5a5bcfb0001cf1a4a10a3f
Moksedul <37239739+Moksedul@users.noreply.github.com>
2020-10-27 08:45:34 +0600

    Version: 1.044

--- main/views.py
+++ main/views.py
@@ -2,12 +2,13 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
+from django.utils.timezone import now
 
 from local_sale.models import LocalSale
 from orders.models import Orders
 from organizations.models import Persons
 from payments.models import Payment
-from payroll.models import Employee
+from payroll.models import Employee, Attendance, Day
 from vouchers.models import SaleVoucher, BuyVoucher
 from stocks.models import Stocks
 
@@ -21,12 +22,23 @@ def home(request):
     buy_count = BuyVoucher.objects.all().count()
     payment_count = Payment.objects.all().count()
     employee_count = Employee.objects.all().count()
+    day = Day.objects.get(date=now())
+    attendances = Attendance.objects.filter(date=day)
+    present = 0
+    absent = 0
+    for attendance in attendances:
+        if attendance.present:
+            present += 1
+        else:
+            absent += 1
     context = {
         'payment_count': payment_count,
         'order_count': order_count,
         'sale_count': sale_count,
         'buy_count': buy_count,
         'employee_count': employee_count,
+        'present': present,
+        'absent': absent,
         'persons': persons,
         'tittle': 'techAlong Business | Home'
     }
