d02948c7af33d9728758467f925c8ef726156d2f
Moksedul Alam <mokseduln@gmail.com>
2020-12-12 17:57:39 +0600

    version 1.2

--- core/digit2words.py
+++ core/digit2words.py
@@ -11,7 +11,7 @@ def num2words(num):
 
     under_20 = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
     tens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-    above_100 = {100: 'Hundred', 1000: 'Thousand', 100000: 'Lakhs', 10000000: 'Crores'}
+    above_100 = {100: 'Hundred', 1000: 'Thousand', 100000: 'Lakhs', 10000000: 'Crore'}
 
     if num < 20:
         return under_20[num]
@@ -22,8 +22,9 @@ def num2words(num):
     # find the appropriate pivot - 'Million' in 3,603,550, or 'Thousand' in 603,550
     pivot = max([key for key in above_100.keys() if key <= num])
 
-    return num2words(num // pivot) + ' ' + above_100[pivot] + ('' if num % pivot==0 else ' ' + num2words(num % pivot))
+    return num2words(num // pivot) + ' ' + above_100[pivot] + ('' if num % pivot == 0 else ' ' + num2words(num % pivot))
 
 
-print(num2words(decimal.Decimal("1054785")))
-# Six Lakhs Fifty Thousand Nine Hundred Fifty Eight point Three Two
+def d2w(digits):
+    number = str(digits)
+    return num2words(decimal.Decimal(number))
