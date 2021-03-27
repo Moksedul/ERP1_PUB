e8bc1d793ba82ebd508af2af263ff52a5f975c91
Moksedul Alam <mokseduln@gmail.com>
2021-03-27 18:29:45 +0600

    version 1.5

--- LC/urls.py
+++ LC/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from LC.views import lc_create, LCList
+from LC.views import lc_create, LCList, LCDelete, lc_update
 
 urlpatterns = [
     path('new_lc', lc_create, name='new-lc'),
     path('lc_list', LCList.as_view(), name='lc-list'),
-    # path('hut_buy/<int:pk>/delete', HutBuyDelete.as_view(), name='delete-hut-buy'),
-    # path('hut_buy/<int:pk>/update', hut_buy_update, name='update-hut-buy'),
+    path('lc/<int:pk>/delete', LCDelete.as_view(), name='delete-lc'),
+    path('lc/<int:pk>/update', lc_update, name='update-lc'),
     # path('hut_buy/<int:pk>/detail', hut_buy_detail, name='detail-hut-buy'),
     # path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
--- LC/views.py
+++ LC/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms import formset_factory
+from django.forms import formset_factory, inlineformset_factory
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.utils.timezone import now
+from django.views.generic import ListView, DeleteView, UpdateView
 
 from LC.forms import LCForm, ProductForm, ExpenseForm
-from LC.models import LC
+from LC.models import LC, LCProduct, LCExpense
 
 
 @login_required
@@ -57,6 +58,63 @@ class LCList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        today = now()
         context['tittle'] = 'techAlong Business|LC List'
-
+        context['today'] = today
+        print(today)
         return context
+
+
+class LCDelete(LoginRequiredMixin, DeleteView):
+    model = LC
+    template_name = 'main/confirm_delete.html'
+    success_url = '/lc_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_name'] = 'LC'
+        context['tittle'] = 'LC Delete'
+        context['cancel_url'] = '/lc_list'
+        return context
+
+
+@login_required
+def lc_update(request, pk):
+    buy = LC.objects.get(pk=pk)
+    form1 = LCForm(instance=buy)
+    ProductFormSet = inlineformset_factory(
+                    LC, LCProduct, fields=('name', 'weight', 'price'), extra=1
+                    )
+    ExpenseFormSet = inlineformset_factory(
+        LC, LCExpense, fields=('name', 'amount',), extra=1
+    )
+    form2set = ProductFormSet(instance=buy)
+    form3set = ExpenseFormSet(instance=buy)
+    if request.method == 'POST':
+        form1 = LCForm(request.POST or None, instance=buy)
+        form2set = ProductFormSet(request.POST, instance=buy)
+        form3set = ExpenseFormSet(request.POST, instance=buy)
+        if form1.is_valid():
+            buy = form1.save(commit=False)
+            buy.posted_by = request.user
+            buy.save()
+            if form2set.is_valid():
+                form2set.save()
+            if form3set.is_valid():
+                form3set.save()
+            return redirect('/hut_buy_list')
+    else:
+        form1 = form1
+        form2set = form2set
+        form3set = form3set
+
+    context = {
+        'form1': form1,
+        'form2set': form2set,
+        'form3set': form3set,
+        'form_name': 'LC Update',
+        'button_name': 'Update',
+        'tittle': 'techAlong Business|LC Update',
+    }
+
+    return render(request, 'hut_buy/hut_buy_form.html', context)
