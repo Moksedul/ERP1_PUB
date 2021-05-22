7215255c0a344c99abcd62b4f380bc6ddc39fd95
Moksedul Alam <mokseduln@gmail.com>
2021-05-22 16:56:51 +0600

    version 1.5

--- stocks/models.py
+++ stocks/models.py
@@ -9,21 +9,21 @@ from vouchers.models import BuyVoucher
 
 
 class Stocks(models.Model):
-    lc = models.ForeignKey(LC, on_delete=models.CASCADE)
-    buy = models.ForeignKey(BuyVoucher, on_delete=models.CASCADE)
-    hut_buy = models.ForeignKey(HutBuy, on_delete=models.CASCADE)
+    lc = models.ForeignKey(LC, null=True, blank=True, on_delete=models.CASCADE)
+    buy = models.ForeignKey(BuyVoucher, null=True, blank=True, on_delete=models.CASCADE)
+    hut_buy = models.ForeignKey(HutBuy, null=True, blank=True, on_delete=models.CASCADE)
     product_name = models.ForeignKey(Products, on_delete=models.CASCADE)
-    weight_loss = models.FloatField()
-    seed_weight = models.FloatField()
-    spot_weight = models.FloatField()
-    seed_rate = models.FloatField()
-    spot_rate = models.FloatField()
+    weight_loss = models.FloatField(default=0)
+    seed_weight = models.FloatField(default=0)
+    spot_weight = models.FloatField(default=0)
+    seed_rate = models.FloatField(default=0)
+    spot_rate = models.FloatField(default=0)
     number_of_bag = models.FloatField()
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     remarks = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return str(self.room_no)
+        return str(self.product_name)
 
     @staticmethod
     def get_absolute_url():
