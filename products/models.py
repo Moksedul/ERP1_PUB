f004eaca5a2d8c22e2347e73509e70c6401606f3
Moksedul <37239739+Moksedul@users.noreply.github.com>
2021-11-02 00:43:04 +0600

    hgjg

--- products/models.py
+++ products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 
-# Create your models here.
 class Products(models.Model):
     product_name = models.CharField(max_length=200)
     product_category = models.CharField(max_length=200)
