9ff270355f2b00749103fd2479bbee263f24d4da
Moksedul Alam <mokseduln@gmail.com>
2021-11-01 17:58:32 +0600

    version 1.5

--- stocks/forms.py
+++ stocks/forms.py
@@ -1,3 +1,4 @@
+from crispy_forms.helper import FormHelper
 from django.forms import ModelForm
 
 from stocks.models import Stocks
@@ -12,3 +13,7 @@ class StockForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['voucher_no'].widget.attrs['disabled'] = 'disabled'
+        self.helper = FormHelper(self)
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
