49a221b95c344e5aa4856b6aa1aa11be4ec313b7
Mokseul Alam <mokseduln@gmail.com>
2022-09-17 13:10:28 +0600

    ready for heroku

--- staticfiles/main/js/responsive.content.height.js
+++
@@ -1,8 +0,0 @@
-//responsive table container height
-$(document).ready(function(){
-	var bodyHeight = $("body").height();
-	var footerHeight = $('.footer').height();
-	var contentHeight = bodyHeight- footerHeight;
-	console.log(contentHeight);
-	$('.table-container').css('height', contentHeight);
-});
